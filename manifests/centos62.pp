2e3dfa2e91cca6ca37d6852c76c59122d8fb46f8
Geoff Davis <gadavis@ucsd.edu>
2012-06-26 11:11:56 -0700

    First working version from baseline vagrant image

--- manifests/centos62.pp
+++ manifests/centos62.pp
@@ -1,1 +1,12 @@
-include 'php'
+if $::osfamily == 'RedHat' {
+  package { 'redhat-lsb' : ensure => installed }
+}
+
+if $::kernel == 'Linux' and $::lsbmajdistrelease {
+  include 'php'
+  include 'apache'
+  include 'php::apache'
+  include 'php::devel'
+} else {
+  notice('The lsbmajdistrelease fact is not available. Not provisioning php or apache on this run')
+}
